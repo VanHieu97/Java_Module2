38fc0e814685f9d520ea9c92b2c07124a65f9dcb
VanHieu97 <vanhieucv1997@gmail.com>
2023-09-06 14:52:39 +0700

    Vòng lặp

--- src/Loop.java
+++ src/Loop.java
@@ -1,5 +1,76 @@
+import java.util.Scanner;
+
 public class Loop {
     public static void main(String[] args) {
+        //Kiểm tra số nguyên tố.
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter a number:");
+//        int number = scanner.nextInt();
+//        if (number < 2) {
+//            System.out.println(number + " is not a prime");
+//        } else {
+//            int i = 2;
+//            boolean check = true;
+//            while (i <= Math.sqrt(number)) {
+//                if (number % i == 0) {
+//                    check = false;
+//                    break;
+//                }
+//                i++;
+//            }
+//            if (check)
+//                System.out.println(number + " is a prime");
+//            else
+//                System.out.println(number + " is not a prime");
+//        }
+        //in ra hình chữ nhật
+//        int length = 5;
+//        int width = 5;
+//        for (int i = 1; i <= length; i++) {
+//            for (int j = 1; j <= width; j++) {
+//                System.out.print(j);
+//            }
+        //System.out.println();
+//        }
+        //in ra hình tam giác
+//        int length = 5;
+//        for (int i = 1; i <= length; i++) {
+//            for (int j = 1; j <= i ; j++) {
+//                System.out.print(j);
+//            }
+        //System.out.println();
+//        }
+// in ra tam giác ngược.
+//        int length = 5;
+//        for (int i = length; i >= 1 ; i--) {
+//            for (int j = 1; j <= i ; j++) {
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        }
+        //in ra số nguyên tố cần in.
+//        Scanner number = new Scanner(System.in);
+//        System.out.println("Nhập vào số lượng số nguyên tố:");
+//        int number1 = number.nextInt();
+//        int count1 = 0;
+//        int count = 0;
+//        int N = 2;
+//        while (count1 < number1) {
+//            count = 0;
+//            for (int i = 1; i <= N; i++) {
+//                if (N % i == 0) {
+//                    count++;
+//                }
+//            }
+//            if (count == 2) {
+//                count1++;
+//                System.out.println(N);
+//            }
+//            N++;
+//        }
 
     }
 }
+
+
+
